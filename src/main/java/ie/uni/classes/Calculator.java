4cbc3b0f785b0e182dc4bba8c357554278dbe4db
rgm <git@rushd.xyz>
2026-01-07 17:04:21 +0000

    Google Java Style: - K&R style braces for class and methods - Block intent by 2 spaces - Indent continuation by 4 spaces - Column limit to 100 characters - Line-wrapping

--- src/main/java/ie/uni/classes/Calculator.java
+++ src/main/java/ie/uni/classes/Calculator.java
@@ -1,88 +1,77 @@
 package ie.uni.classes;
 
-public class Calculator
-{
-    private final double FIRST_NUMBER;
-    private final double SECOND_NUMBER;
-    private final String USER_CHOICE;
+public class Calculator {
+  private final double FIRST_NUMBER;
+  private final double SECOND_NUMBER;
+  private final String USER_CHOICE;
 
-    public Calculator(double firstNum, double secondNum, String userChoice)
-    {
-        this.FIRST_NUMBER = firstNum;
-        this.SECOND_NUMBER = secondNum;
-        this.USER_CHOICE = userChoice;
-    }
+  public Calculator(double firstNum, double secondNum, String userChoice) {
+    this.FIRST_NUMBER = firstNum;
+    this.SECOND_NUMBER = secondNum;
+    this.USER_CHOICE = userChoice;
+  }
 
-    public double getFIRST_NUMBER()
-    {
-        return FIRST_NUMBER;
-    }
+  public double getFIRST_NUMBER() {
+    return FIRST_NUMBER;
+  }
 
-    public double getSECOND_NUMBER()
-    {
-        return SECOND_NUMBER;
-    }
+  public double getSECOND_NUMBER() {
+    return SECOND_NUMBER;
+  }
 
-    public String getUSER_CHOICE()
-    {
-        return USER_CHOICE;
-    }
+  public String getUSER_CHOICE() {
+    return USER_CHOICE;
+  }
 
-    // selects operation for two passed numbers and prints result
-    void operation() {
-        String result = switch (getUSER_CHOICE()) {
-            case "+" -> Double.toString(add());
-            case "-" -> Double.toString(sub());
-            case "*" -> Double.toString(mul());
-            case "/" -> {
-                if (getSECOND_NUMBER() == 0) {
-                    System.out.println("To infinity and beyond, as the second number = 0!");
-                    yield "\uD83D\uDE80";
-                } else {
-                    yield Double.toString(div());
-                }
-            }
-            case "^" -> Double.toString(pow());
-            case "%" -> Double.toString(mod());
-            default -> {
-                System.out.println("Invalid operation selected, please type \"+\" to add, \"-\" "
-                    + "to subtract, \"*\" to multiply or \"/\" to divide next time.");
-                yield "\uD83E\uDD37???";
-            }
-        };
-        System.out.println(getFIRST_NUMBER() + " " + getUSER_CHOICE() + " " + getSECOND_NUMBER()
-            + " = " + result);
-    }
+  // selects operation for two passed numbers and prints result
+  void operation() {
+    String result = switch (getUSER_CHOICE()) {
+      case "+" -> Double.toString(add());
+      case "-" -> Double.toString(sub());
+      case "*" -> Double.toString(mul());
+      case "/" -> {
+        if (getSECOND_NUMBER() == 0) {
+          System.out.println("To infinity and beyond, as the second number = 0!");
+          yield "\uD83D\uDE80";
+        } else {
+          yield Double.toString(div());
+        }
+      }
+      case "^" -> Double.toString(pow());
+      case "%" -> Double.toString(mod());
+      default -> {
+        System.out.println("Invalid operation selected, please type \"+\" to add, \"-\" "
+            + "to subtract, \"*\" to multiply or \"/\" to divide next time.");
+        yield "\uD83E\uDD37???";
+      }
+    };
+    System.out.println(getFIRST_NUMBER() + " " + getUSER_CHOICE() + " " + getSECOND_NUMBER()
+        + " = " + result);
+  }
 
-    double add()
-    {
-        return getFIRST_NUMBER() + getSECOND_NUMBER();
-    }
+  double add() {
+    return getFIRST_NUMBER() + getSECOND_NUMBER();
+  }
 
-    double sub()
-    {
-        return getFIRST_NUMBER() - getSECOND_NUMBER();
-    }
+  double sub() {
+    return getFIRST_NUMBER() - getSECOND_NUMBER();
+  }
 
-    double mul()
-    {
-        return getFIRST_NUMBER() * getSECOND_NUMBER();
-    }
+  double mul() {
+    return getFIRST_NUMBER() * getSECOND_NUMBER();
+  }
 
-    double div()
-    {
-        return getFIRST_NUMBER() / getSECOND_NUMBER();
-    }
+  double div() {
+    return getFIRST_NUMBER() / getSECOND_NUMBER();
+  }
 
-    // exponent two numbers - raise the first number to the power of the second
-    double pow()
-    {
-        return Math.pow(getFIRST_NUMBER(), getSECOND_NUMBER());
-    }
+  // exponent two numbers - raise the first number to the power of the second
+  double pow() {
+    return Math.pow(getFIRST_NUMBER(), getSECOND_NUMBER());
+  }
 
-    // modulo two numbers - return the remainder of the first number divided by the second
-    double mod()
-    {
-        return getFIRST_NUMBER() % getSECOND_NUMBER();
-    }
+  // modulo two numbers - return the remainder of the first number divided by the second
+  double mod() {
+    return getFIRST_NUMBER() % getSECOND_NUMBER();
+  }
 }
